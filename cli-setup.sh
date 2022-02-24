8f005caae2d0c1e47df372c40e916bcb23c01534
softskillpro <softskill.pro20@gmail.com>
2022-02-24 07:32:42 +0000

    Updated install script for cli-tool

--- cli-setup.sh
+++ cli-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+mkdir sacred-trees-snarks
+cd sacred-trees-snarks
 echo "Downloading Circuit Data..."
 wget https://sacred-trees-snarks.s3.us-east-2.amazonaws.com/sacred-trees-snarks.tar.gz
 tar -xf sacred-trees-snarks.tar.gz
+cd ..
 echo "Installing node modules for cli-tool..."
 yarn install
 echo "Done!"
