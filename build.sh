da4e950d7334dd08f8ad374c2174386e6ae2e3b9
softskillpro <softskill.pro20@gmail.com>
2022-03-23 04:33:58 +0000

    Update build process for light version of circuits for sacred-treep root update

--- build.sh
+++ build.sh
@@ -2,6 +2,22 @@
 
 # expecting node v12
 
+declare -A envs
+function load_envs() {
+  local envFile=${1:-.env}
+  local isComment='^[[:space:]]*#'
+  local isBlank='^[[:space:]]*$'
+  while IFS= read -r line; do
+    [[ $line =~ $isComment ]] && continue
+    [[ $line =~ $isBlank ]] && continue
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    envs["$key"]=$value
+  done < <( cat "$envFile" )
+}
+
+load_envs
+
 cd sacred-token
 yarn unlink || true
 yarn link
@@ -18,7 +34,15 @@ cd ..
 cd sacred-trees
 yarn
 #yarn circuit
-cp ./snarks/BatchTreeUpdateVerifier.sol ./artifacts/circuits/BatchTreeUpdateVerifier.sol
+if [ ${envs["SMALL_BATCH_SIZE_ROOT_UPDATE"]} = 1 ]
+  then
+    yarn changeTreeHeight 2
+    cp ./snarks_light/BatchTreeUpdateVerifier.sol ./artifacts/circuits/BatchTreeUpdateVerifier.sol
+  else
+    yarn changeTreeHeight 8
+    cp ./snarks/BatchTreeUpdateVerifier.sol ./artifacts/circuits/BatchTreeUpdateVerifier.sol
+fi
+
 yarn compile
 cd ..
 
