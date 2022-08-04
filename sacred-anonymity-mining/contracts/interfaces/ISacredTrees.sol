1e6136b276840a33d9f7666d36f6f8d4077f7f38
softskillpro <softskill.pro20@gmail.com>
2022-08-04 05:43:39 +0000

    Fixed bugs

--- sacred-anonymity-mining/contracts/interfaces/ISacredTrees.sol
+++ sacred-anonymity-mining/contracts/interfaces/ISacredTrees.sol
@@ -4,5 +4,5 @@ pragma solidity 0.8.9;
 interface ISacredTrees {
   function registerDeposit(address instance, bytes32 commitment) external;
   function registerWithdrawal(address instance, bytes32 nullifier) external;
-  function validateRoots(bytes32 _depositRoot, bytes32 _withdrawalRoot) public view;
+  function validateRoots(bytes32 _depositRoot, bytes32 _withdrawalRoot) external view;
 }
