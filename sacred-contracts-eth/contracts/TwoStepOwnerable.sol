cdfd24026a5de7409a7e57db0ca3c8aed40d4ad5
softskillpro <softskill.pro20@gmail.com>
2022-08-04 23:01:00 +0000

    added TwoStepOwnerable

---
+++ sacred-contracts-eth/contracts/TwoStepOwnerable.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+abstract contract TwoStepOwnerable {
+  address internal owner;
+  mapping (address => bool) private invited;
+	event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+	
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not authorized");
+    _;
+  }
+
+  modifier onlyInvited() {
+  	require(invited[msg.sender] == true, "Not authorized");
+    _;
+  }
+
+  constructor(address _owner) {
+    owner = _owner;
+  }
+  
+  function transferOwnership(address newOwner) public onlyOwner {
+	  require(newOwner != address(0), "owner cannot be zero address");
+  	invited[newOwner] = true;
+  }
+  
+  function ownershipAccepted() public onlyInvited {
+	  _transferOwnership(msg.sender);
+  	delete invited[msg.sender];
+  }
+  
+  function renounceOwnership() public onlyOwner {
+    _transferOwnership(address(0));
+  }
+  
+  function _transferOwnership(address newOwner) internal {
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
