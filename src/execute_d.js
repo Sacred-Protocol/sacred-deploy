da88937cc8f020f210aa2f616c13f0e8bda10b24
softskillpro <softskill.pro20@gmail.com>
2022-08-30 10:05:03 +0000

    applied multicall

--- src/execute_d.js
+++ src/execute_d.js
@@ -1,31 +1,37 @@
 require('dotenv').config()
 const fs = require('fs')
+const { ethers } = require("hardhat")
 const utils = require('../sacred-contracts-eth/lib/utils')
-const { updateAddressTable} = require('../lib/deployUtils')
+const { ensToAddr, updateAddressTable} = require('../lib/deployUtils')
 const instancesInfo = require('../config.json')
+const config = require('../sacred-token/config')
 const addressTable = require('../address.json')
-const ethSacredAbi = require('./abi/ETHSacred.json')
+const ethSacredAbi = require('../abi/ETHSacred.json')
 const erc20Abi = require('../abi/erc20.abi.json')
 const erc20SacredAbi = require('../abi/ERC20Sacred.json')
-const withdrawCircuit = require('../sacred-contracts-eth/build/circuits/withdraw.json')
-const withdrawProvidingKey = fs.readFileSync('sacred-contracts-etc/build/circuits/withdraw_proving_key.bin').buffer
+const sacredProxyAbi = require('../sacred-anonymity-mining/artifacts/contracts/SacredProxy.sol/SacredProxy.json')
+const provingKeys = {
+  wasmPath: "./sacred-contracts-eth/build/circuits/withdraw_js/withdraw.wasm",
+  zkeyFilePath: "./sacred-contracts-eth/build/circuits/withdraw_0001.zkey",
+}
 
 const {  RPC_URL } = process.env
 
 async function main() {
-  await utils.init({instancesInfo, erc20Contract: erc20Abi, RPC_URL})
-
+  await utils.init({ instancesInfo, erc20Contract: erc20Abi, rpc:RPC_URL })
+  const wallet = utils.getWalllet()
   updateAddressTable(addressTable)
-  const signers = await ethers.getSigners();
-  owner = signers[0];
+  const sacredProxy = new ethers.Contract(ensToAddr(config.sacredProxy.address), sacredProxyAbi.abi, wallet)
+ 
   await utils.setup({
-    ethSacredAbi: ethSacredAbi.abi, 
-    erc20SacredAbi, 
-    withdrawCircuit,
-    withdrawProvidingKey
+    ethSacredAbi: ethSacredAbi.abi,
+    erc20SacredAbi: erc20SacredAbi.abi,
+    sacredProxyContract: sacredProxy,
+    wasmPath: provingKeys.wasmPath,
+    zkeyFilePath: provingKeys.zkeyFilePath
   });
-
-  for(let i = 0; i < 2; i++) {
+  const tokenAddress = instancesInfo.pools[`${utils.getNetId()}`]["dai"].token
+  for(let i = 0; i < 256; i++) {
     //Deposit
     console.log("Count Number:", i);
     fs.writeFile('result.txt', '' + i, function (err) {
@@ -33,8 +39,9 @@ async function main() {
     });
     let ethbalance = 0;
     try {
-      ethbalance = Number(ethers.utils.formatEther(await owner.getBalance()));
-      console.log('User ETH balance is ', ethbalance);
+      utils.printERC20Balance({address: wallet.address, name:"dai", tokenAddress})
+      //ethbalance = Number(ethers.utils.formatEther(await wallet.getBalance()));
+      //console.log('User ETH balance is ', ethbalance);
     } catch {
       i--;
       continue;
@@ -42,7 +49,7 @@ async function main() {
 
     let noteString = "";
     try {
-      const result = await utils.deposit({currency:'eth', amount:0.1});
+      const result = await utils.deposit({currency:'dai', amount:200});
       console.log("depositBlockNum = ", result.blockNumber);
       noteString = result.noteString;
     } catch(e) {
@@ -54,8 +61,8 @@ async function main() {
     //Withdraw
     while(noteString !== "") {
       try {
-        let data = utils.parseNote(noteString);
-        const withdrawBlockNumber = await utils.withdraw({ deposit: data.deposit, currency: data.currency, amount:data.amount, recipient: owner.address, relayerURL: null });
+        const { currency, amount, netId, deposit } = utils.baseUtils.parseNote(noteString)
+        const withdrawBlockNumber = await utils.withdraw({ deposit, currency, amount, recipient: wallet.address, relayerURL: null });
         console.log("withdrawBlockNum = ", withdrawBlockNumber);
         noteString = "";
       } catch(e) {
